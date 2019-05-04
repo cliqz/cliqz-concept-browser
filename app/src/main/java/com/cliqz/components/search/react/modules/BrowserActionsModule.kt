a6c87f9761f3f88b8a09a0e521f97a6d0aa79896
Sam Macbeth <sam@macbeth.cc>
2019-05-04 11:03:24 +0200

    Implement history search

--- app/src/main/java/com/cliqz/components/search/react/modules/BrowserActionsModule.kt
+++ app/src/main/java/com/cliqz/components/search/react/modules/BrowserActionsModule.kt
@@ -3,6 +3,7 @@ package com.cliqz.components.search.react.modules
 import com.facebook.react.bridge.*
 import com.facebook.react.bridge.ReactMethod
 import android.content.Context
+import android.os.Bundle
 import org.mozilla.reference.browser.ext.components
 
 class BrowserActionsModule(reactContext: ReactApplicationContext, val context: Context) : ReactContextBaseJavaModule(reactContext) {
@@ -12,8 +13,20 @@ class BrowserActionsModule(reactContext: ReactApplicationContext, val context: C
 
     @ReactMethod
     fun searchHistory(query: String, callback: Callback) {
-        val wArray = Arguments.createArray()
-        callback.invoke(wArray)
+        if (query != null && query.length > 1) {
+            val results = context.components.core.historyStorage.getSuggestions(query, 5)
+                    .map { searchResult ->
+                val map = Bundle()
+                map.putString("id", searchResult.id)
+                map.putString("url", searchResult.url)
+                map.putString("title", searchResult.title)
+                map.putInt("score", searchResult.score)
+                map
+            }
+            callback.invoke(Arguments.fromList(results))
+        } else {
+            callback.invoke(Arguments.createArray())
+        }
     }
 
 
